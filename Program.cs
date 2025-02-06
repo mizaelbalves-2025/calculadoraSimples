bc61005403107ab039908393d8fde61965689c42
MIZAEL BARROS ALVES <mizael.balves@gmail.com>
2025-02-06 11:26:34 -0300

    Double 06-02-25

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace calculadoraSimples
         static void Main(string[] args)
         {
             Console.WriteLine("Digite o primeiro número");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Digite o segundo número");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine($"A soma do {num1} e {num2} é {num1 + num2}");
             Console.WriteLine($"A subitração do {num1} e {num2} é {num1 - num2}");
