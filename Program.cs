5debe835d9b6ef8adaf6fc6ba4a94780ef73f5ae
MIZAEL BARROS ALVES <mizael.balves@gmail.com>
2025-02-06 11:22:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A soma do {num1} e {num2} é {num1 + num2}");
+            Console.WriteLine($"A subitração do {num1} e {num2} é {num1 - num2}");
+            Console.WriteLine($"A muntiplicação do {num1} e {num2} é {num1 * num2}");
+            Console.WriteLine($"A divisão do {num1} e {num2} é {num1 / num2}");
+        }  
+
+    }
+}
